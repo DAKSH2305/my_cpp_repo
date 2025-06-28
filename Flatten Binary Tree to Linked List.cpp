0f7a6fec72aedd7d2422b250e8b3eeabc6275b59
Daksh Jain <daksh.24bce10620@vitbhopal.ac.in>
2025-06-28 06:30:24 +0530

    Create Flatten Binary Tree to Linked List.cpp

---
+++ Flatten Binary Tree to Linked List.cpp
@@ -0,0 +1,39 @@
+// 114. Flatten Binary Tree to Linked List
+// link - https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/?envType=problem-list-v2&envId=binary-tree
+/*
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+*/
+
+
+class Solution {
+    void preorder(TreeNode*root,vector<TreeNode*> &v){
+        if(root==NULL){
+            
+            return ;
+
+        }
+        v.push_back(root);
+        preorder(root->left,v);
+        preorder(root->right,v);
+    }
+public:
+    void flatten(TreeNode* root) {
+        if(root==NULL ){
+            return;
+        }
+       
+        vector<TreeNode*> sol;
+        preorder(root,sol);
+        for(int i=0;i<sol.size()-1;i++){
+            
+            sol[i]->left=NULL;
+            sol[i]->right=sol[i+1];
+        }
+        sol.back()->left=NULL;
+        sol.back()->right=NULL;
+
+    }
+};
